794c0c34bab010c896e5ea5acce95f067a5cc82c
Rendy Ariawindana <rendy.ariawindana@gmail.com>
2018-04-08 00:33:59 +0700

    tambah import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Api from 'superagent';
+
+import {BootstrapTable,
+  TableHeaderColumn} from 'react-bootstrap-table';
+import {Line as LineChart} from 'react-chartjs';
+
+import logo from './image/logo.png';
+import rice from './image/rice.svg';
+
+import './bower_components/bootstrap/dist/css/bootstrap.min.css';
+import './bower_components/font-awesome/css/font-awesome.min.css';
+import './bower_components/Ionicons/css/ionicons.min.css';
+import './dist/css/AdminLTE.css';
+import './dist/css/skins/_all-skins.min.css';
 
 class App extends Component {
   render() {
